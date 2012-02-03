d3f4c542813deb590eaad99b2884ccf78a50766e
Jason Kraus <jasonk@cukerinteractive.com>
2012-02-03 13:38:54 -0800

    refactored temp document

--- dockit/models.py
+++ dockit/models.py
@@ -3,8 +3,57 @@ from dockit.backends import get_document_backend
 
 from copy import deepcopy
 
+class SchemaProxyDict(dict):
+    def __getitem__(self, key):
+        val = dict.__getitem__(self, key)
+        return TemporaryDocument.generate_document(val)
+
 class TemporaryDocument(dockit.Document):
-    pass
+    @classmethod
+    def generate_document(cls, document):
+        class GeneratedTempDocument(cls):
+            class Meta:
+                proxy = True
+        fields = deepcopy(document._meta.fields)
+        
+        #handle dynamic typing
+        if document._meta.typed_field:
+            GeneratedTempDocument._meta.typed_field = document._meta.typed_field
+            GeneratedTempDocument._meta.typed_key = document._meta.typed_key
+            t_field = fields[document._meta.typed_field]
+            orignal_schemas = t_field.schemas
+            t_field.schemas = SchemaProxyDict(orignal_schemas)
+        
+        GeneratedTempDocument._meta.fields.update(fields)
+        GeneratedTempDocument._meta.original_document = document
+        
+        for name, value in GeneratedTempDocument._meta.fields.iteritems():
+            if hasattr(value, 'contribute_to_class'):
+                value.contribute_to_class(GeneratedTempDocument, name)
+            else:
+                setattr(GeneratedTempDocument, name, value)
+        
+        return GeneratedTempDocument
+    
+    def commit_changes(self, doc_id=None):
+        document_cls = self._meta.original_document
+        
+        backend = get_document_backend()
+        id_field = backend.get_id_field_name()
+        
+        data = self.to_primitive(self)
+        data[id_field] = doc_id
+        
+        instance = document_cls(_primitive_data=data)
+        instance.save()
+        return instance
+    
+    def copy_from_instance(self, instance):
+        backend = get_document_backend()
+        data = instance.to_primitive(instance)
+        self._original_id = data.pop(backend.get_id_field_name(), None)
+        self._primitive_data = data
+        self._python_data = dict()
 
 class FactoryMeta(object):
     def __init__(self, attributes):
@@ -12,62 +61,5 @@ class FactoryMeta(object):
             setattr(self, key, value)
 
 def create_temporary_document_class(document_cls):
-    #TODO refactor to something that simply copies the fields and handles dynamic typing
-    class TempDocument(document_cls):
-        class Meta:
-            virtual = True
-            proxy = True
-            collection = TemporaryDocument._meta.collection
-        
-        def __init__(self, *args, **kwargs):
-            dockit.Document.__init__(self, *args, **kwargs)
-        
-        def commit_changes(self, doc_id=None):
-            backend = get_document_backend()
-            id_field = backend.get_id_field_name()
-            
-            data = self.to_primitive(self)
-            data[id_field] = doc_id
-            if hasattr(self, '_original_id'):
-                data[id_field] = self._original_id
-            
-            instance = document_cls(_primitive_data=data)
-            instance.save()
-            return instance
-        
-        def copy_from_instance(self, instance):
-            backend = get_document_backend()
-            data = instance.to_primitive(instance)
-            self._original_id = data.pop(backend.get_id_field_name(), None)
-            self._primitive_data = data
-            self._python_data = dict()
-        
-        @classmethod
-        def to_python(cls, val, parent=None):
-            if val is None:
-                val = dict()
-            if cls._meta.typed_field:
-                field = cls._meta.fields[cls._meta.typed_field]
-                key = val.get(cls._meta.typed_field, None)
-                if key:
-                    cls = field.schemas[key]
-                    cls = create_temporary_document_class(cls)
-            return cls(_primitive_data=val, _parent=parent)
-        
-        def save(self, *args, **kwargs):
-            assert self._meta.collection == TemporaryDocument._meta.collection
-            return dockit.Document.save(self, *args, **kwargs)
-        
-        def delete(self, *args, **kwargs):
-            assert self._meta.collection == TemporaryDocument._meta.collection
-            return dockit.Document.delete(self, *args, **kwargs)
-    
-    #TODO investigate: Exception RuntimeError: 'maximum recursion depth exceeded in __subclasscheck__' in <type 'exceptions.AttributeError'> ignored
-    TempDocument._meta.fields = deepcopy(document_cls._meta.fields)
-    for name, value in TempDocument._meta.fields.iteritems():
-        if hasattr(value, 'contribute_to_class'):
-            value.contribute_to_class(TempDocument, name)
-        else:
-            setattr(TempDocument, name, value)
-    
-    return TempDocument
+    return TemporaryDocument.generate_document(document_cls)
+
