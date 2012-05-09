6ec90e11eb93e45029655e62672f083e32641052
Jason Kraus <jasonk@cukerinteractive.com>
2012-05-09 11:50:09 -0700

    test against 2.5

--- dockit/forms/forms.py
+++ dockit/forms/forms.py
@@ -254,7 +254,8 @@ class BaseDocumentForm(BaseForm):
             value = field.widget.value_from_datadict(self.data, self.files, self.add_prefix(name))
             try:
                 arg_spec = inspect.getargspec(field.clean)
-                if len(arg_spec.args) > 2:
+                if ((isinstance(arg_spec, tuple) and len(arg_spec[0]) > 2) or #py2.5
+                    len(arg_spec.args) > 2): #py2.6+
                     initial = self.initial.get(name, field.initial)
                     if callable(initial):
                         initial = initial()
