dbc2bb24a201c3c37675d92a8d7462c363be94e7
Jason Kraus <jasonk@cukerinteractive.com>
2012-06-26 13:52:19 -0700

    manifest classes manage loading of data sources

--- dockit/manifest/common.py
+++ dockit/manifest/common.py
@@ -38,22 +38,10 @@ class ManifestLoader(object):
     def load_manifest(self, manifest_data):
         manifest_options = dict(manifest_data)
         manifest_cls = self.manifests[manifest_options.pop('loader')]
-        data_sources = list()
-        for data in manifest_options.pop('data'):
-            data_source = self.load_data_source(data)
-            data_sources.append(data_source)
-        manifest_kwargs = self.get_manifest_kwargs(data_sources=data_sources, **manifest_options)
-        return manifest_cls(**manifest_kwargs)
-    
-    def get_data_source_kwargs(self, **kwargs):
-        return kwargs
-    
-    def load_data_source(self, data):
-        data_source_options = dict(data)
-        data_source_cls = self.data_sources[data_source_options.pop('source')]
-        data_source_kwargs = self.get_data_source_kwargs(**data_source_options)
-        data_source = data_source_cls(**data_source_kwargs)
-        return data_source
+        manifest_kwargs = self.get_manifest_kwargs(**manifest_options)
+        manifest = manifest_cls(**manifest_kwargs)
+        manifest.load_data_sources(self)
+        return manifest
     
     def create_manifest_payload(self, loader, data_sources):
         '''
@@ -73,9 +61,24 @@ class ManifestLoader(object):
         return payload
 
 class Manifest(object):
-    def __init__(self, data_sources, **options):
-        self.data_sources = data_sources
+    def __init__(self, **options):
+        self.data_sources = list()
         self.options = options
+    
+    def load_data_sources(self, loader):
+        for data in self.options.pop('data', []):
+            self.load_data_source(loader, data)
+    
+    def get_data_source_kwargs(self, **kwargs):
+        return kwargs
+    
+    def load_data_source(self, loader, data):
+        data_source_options = dict(data)
+        data_source_cls = loader.data_sources[data_source_options.pop('source')]
+        data_source_kwargs = self.get_data_source_kwargs(**data_source_options)
+        data_source = data_source_cls(**data_source_kwargs)
+        self.data_sources.append(data_source)
+        return data_source
     
     def load(self):
         '''
