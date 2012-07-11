c037ea7605e6c9e318caa69473daa1c304c9437d
Jason Kraus <jasonk@cukerinteractive.com>
2012-07-10 17:00:06 -0700

    serializers code cleanup

--- dockit/core/serializers/base.py
+++ dockit/core/serializers/base.py
@@ -4,17 +4,7 @@ Module for abstract serializer/unserializer base classes.
 
 from StringIO import StringIO
 
-from django.db import models
-from django.utils.encoding import smart_str, smart_unicode
-from django.utils import datetime_safe
-
-class SerializationError(Exception):
-    """Something bad happened during serialization."""
-    pass
-
-class DeserializationError(Exception):
-    """Something bad happened during deserialization."""
-    pass
+from django.core.serializers.base import SerializationError, DeserializationError
 
 class Serializer(object):
     """
@@ -32,33 +22,14 @@ class Serializer(object):
         self.options = options
 
         self.stream = options.pop("stream", StringIO())
-        self.selected_fields = options.pop("fields", None)
         self.use_natural_keys = options.pop("use_natural_keys", False)
 
         self.start_serialization()
         for obj in queryset:
             self.start_object(obj)
-            for field in obj._meta.local_fields:
-                if field.serialize:
-                    if field.rel is None:
-                        if self.selected_fields is None or field.attname in self.selected_fields:
-                            self.handle_field(obj, field)
-                    else:
-                        if self.selected_fields is None or field.attname[:-3] in self.selected_fields:
-                            self.handle_fk_field(obj, field)
-            for field in obj._meta.many_to_many:
-                if field.serialize:
-                    if self.selected_fields is None or field.attname in self.selected_fields:
-                        self.handle_m2m_field(obj, field)
             self.end_object(obj)
         self.end_serialization()
         return self.getvalue()
-
-    def get_string_value(self, obj, field):
-        """
-        Convert a field's value to a string.
-        """
-        return smart_unicode(field.value_to_string(obj))
 
     def start_serialization(self):
         """
@@ -83,24 +54,6 @@ class Serializer(object):
         Called when serializing of an object ends.
         """
         pass
-
-    def handle_field(self, obj, field):
-        """
-        Called to handle each individual (non-relational) field on an object.
-        """
-        raise NotImplementedError
-
-    def handle_fk_field(self, obj, field):
-        """
-        Called to handle a ForeignKey field.
-        """
-        raise NotImplementedError
-
-    def handle_m2m_field(self, obj, field):
-        """
-        Called to handle a ManyToManyField.
-        """
-        raise NotImplementedError
 
     def getvalue(self):
         """
@@ -127,7 +80,7 @@ class Deserializer(object):
         # hack to make sure that the models have all been loaded before
         # deserialization starts (otherwise subclass calls to get_model()
         # and friends might fail...)
-        models.get_apps()
+        #models.get_apps()
 
     def __iter__(self):
         return self
@@ -138,25 +91,23 @@ class Deserializer(object):
 
 class DeserializedObject(object):
     """
-    A deserialized model.
+    A deserialized document.
 
-    Basically a container for holding the pre-saved deserialized data along
-    with the many-to-many data saved with the object.
+    Basically a container for holding the pre-saved deserialized data.
 
     Call ``save()`` to save the object (with the many-to-many data) to the
     database; call ``save(save_m2m=False)`` to save just the object fields
     (and not touch the many-to-many stuff.)
     """
 
-    def __init__(self, obj, m2m_data=None):
+    def __init__(self, obj):
         self.object = obj
-        self.m2m_data = m2m_data
 
     def __repr__(self):
         return "<DeserializedObject: %s.%s(pk=%s)>" % (
             self.object._meta.app_label, self.object._meta.object_name, self.object.pk)
 
-    def save(self, save_m2m=True, using=None):
+    def save(self):
         # Call save on the Model baseclass directly. This bypasses any
         # model-defined save. The save is also forced to be raw.
         # This ensures that the data that is deserialized is literally
@@ -164,6 +115,3 @@ class DeserializedObject(object):
         # methods.
         self.object.save()
 
-        # prevent a second (possibly accidental) call to save() from saving
-        # the m2m data twice.
-        self.m2m_data = None
--- dockit/core/serializers/python.py
+++ dockit/core/serializers/python.py
@@ -4,9 +4,7 @@ and from basic Python data types (lists, dicts, strings, etc.). Useful as a basi
 other serializers.
 """
 
-from django.conf import settings
-from django.db import models, DEFAULT_DB_ALIAS
-from django.utils.encoding import smart_unicode, is_protected_type
+from django.utils.encoding import smart_unicode
 
 from dockit.core.serializers import base
 from dockit.schema.loading import get_base_document
@@ -26,7 +24,7 @@ class Serializer(base.Serializer):
         pass
 
     def start_object(self, obj):
-        self._current = {}
+        self._current = obj.to_portable_primitive(obj)
 
     def end_object(self, obj):
         self.objects.append({
@@ -35,39 +33,6 @@ class Serializer(base.Serializer):
             "fields" : self._current
         })
         self._current = None
-
-    def handle_field(self, obj, field):
-        value = field._get_val_from_obj(obj)
-        # Protected types (i.e., primitives like None, numbers, dates,
-        # and Decimals) are passed through as is. All other values are
-        # converted to string first.
-        if is_protected_type(value):
-            self._current[field.name] = value
-        else:
-            self._current[field.name] = field.value_to_string(obj)
-
-    def handle_fk_field(self, obj, field):
-        related = getattr(obj, field.name)
-        if related is not None:
-            if self.use_natural_keys and hasattr(related, 'natural_key'):
-                related = related.natural_key()
-            else:
-                if field.rel.field_name == related._meta.pk.name:
-                    # Related to remote object via primary key
-                    related = related._get_pk_val()
-                else:
-                    # Related to remote object via other field
-                    related = smart_unicode(getattr(related, field.rel.field_name), strings_only=True)
-        self._current[field.name] = related
-
-    def handle_m2m_field(self, obj, field):
-        if field.rel.through._meta.auto_created:
-            if self.use_natural_keys and hasattr(field.rel.to, 'natural_key'):
-                m2m_value = lambda value: value.natural_key()
-            else:
-                m2m_value = lambda value: smart_unicode(value._get_pk_val(), strings_only=True)
-            self._current[field.name] = [m2m_value(related)
-                               for related in getattr(obj, field.name).iterator()]
 
     def getvalue(self):
         return self.objects
@@ -79,31 +44,12 @@ def Deserializer(object_list, **options):
     It's expected that you pass the Python objects themselves (instead of a
     stream or a string) to the constructor
     """
-    db = options.pop('using', DEFAULT_DB_ALIAS)
-    models.get_apps()
+    #models.get_apps()
     for d in object_list:
         # Look up the model and starting build a dict of data for it.
-        Model = get_base_document(d["model"])
-        data = {Model._meta.pk.attname : Model._meta.pk.to_python(d["pk"])}
-        m2m_data = {}
+        doc_cls = get_base_document(d["model"])
+        data = {doc_cls._meta.pk.attname : doc_cls._meta.pk.to_python(d["pk"])}
+        data.update(d['fields'])
+        
+        yield base.DeserializedObject(doc_cls.from_portable_primitive(data))
 
-        # Handle each field
-        for (field_name, field_value) in d["fields"].iteritems():
-            if isinstance(field_value, str):
-                field_value = smart_unicode(field_value, options.get("encoding", settings.DEFAULT_CHARSET), strings_only=True)
-
-            data[field_name] = field_value
-
-        yield base.DeserializedObject(Model.to_python(data), m2m_data)
-
-def _get_model(model_identifier):
-    """
-    Helper to look up a model from an "app_label.module_name" string.
-    """
-    try:
-        Model = models.get_model(*model_identifier.split("."))
-    except TypeError:
-        Model = None
-    if Model is None:
-        raise base.DeserializationError(u"Invalid model identifier: '%s'" % model_identifier)
-    return Model
