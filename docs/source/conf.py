d13955c364cd9317bc22fe65ba51004a75ef6c13
Jason Kraus <jasonk@cukerinteractive.com>
2012-09-17 14:47:17 -0700

    updated doc [skip ci]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Django Fieldmaker'
+project = u'Django Dockit'
 copyright = u'2011'
 
 # The version info for the project you're documenting, acts as replacement for
