0005858cf89185cf30b2aac4ee32b3498c6ad388
Jason Kraus <zbyte64@gmail.com>
2013-03-31 18:04:23 -0700

    updated datatap

--- dockit/datataps.py
+++ dockit/datataps.py
@@ -1,33 +1,5 @@
-'''
-Example usage::
-
-    from datatap.dataps import JSONStreamDataTap, ZipFileDataTap
-    from dockit.datataps import DocumentDataTap
-    
-    #serialize documents
-    outstream = JSONStreamDataTap(stream=sys.stdout)
-    outstream.open('w'. for_datatap=ModelDataTap)
-    source = DocumentDataTap(MyDocument, Blog.objects.filter(is_active=True))
-    source.dump(outstream)
-    
-    instream = JSONStreamDataTap(stream=open('fixture.json', 'r'))
-    DocumentDataTap.load(instream)
-    
-    #give me all active users to stdout
-    DocumentDataTap.store(JSONStreamDataTap(stream=sys.stdout), User.objects.filter(is_active=True))
-    
-    #write Blog and BlogImages to a zipfile
-    archive = ZipFileDataTap(filename='myblog.zip')
-    archive.open('w', for_datatap=DocumentDataTap)
-    #or do it in one line: archive = ZipFileDataTap(filename='myblog.zip', mode='w', for_datatap=DocumentDataTap)
-    DocumentDataTap.store(archive, Blog, BlogImages)
-    archive.close()
-    
-    archive = ZipFileDataTap(filename='myblog.zip', mode='r')
-    DocumentDataTap.load(archive)
-
-'''
 from optparse import OptionParser
+from collections import deque
 
 from django.core.files import File
 
@@ -35,56 +7,37 @@ from dockit import schema
 from dockit.schema.loading import get_documents, get_document
 from dockit.core.serializers.python import Serializer, Deserializer
 
-
-from datatap.encoders import ObjectIteratorAdaptor
 from datatap.loading import register_datatap
-from datatap.datataps.base import DataTap, WriteStream
+from datatap.datataps.base import DataTap
 
-
-class DocumentWriteStream(WriteStream):
-    def __init__(self, datatap, itemstream):
-        super(DocumentWriteStream, self).__init__(datatap, Deserializer(itemstream))
-    
-    def process_item(self, item):
-        #item is a deserialized object
-        item.save()
-        return item.object
 
 class FileAwareSerializer(Serializer):
     def start_object(self, obj):
         #TODO set files in self._current
         return super(FileAwareSerializer, self).start_object(obj)
 
-class DocumentIteratorAdaptor(ObjectIteratorAdaptor):
-    def __init__(self, use_natural_keys=True, **kwargs):
-        self.serializer = FileAwareSerializer()
-        self.use_natural_keys = use_natural_keys
-        super(DocumentIteratorAdaptor, self).__init__(**kwargs)
-    
-    def transform(self, obj):
-        #TODO not thread safe
-        self.serializer.serialize([obj], use_natural_keys=self.use_natural_keys)
-        return self.serializer.objects.pop()
-
 class DocumentDataTap(DataTap):
     '''
     Reads and writes from DocKit's Collections
     '''
-    write_stream_class = DocumentWriteStream
-    object_iterator_class = DocumentIteratorAdaptor
+    def __init__(self, instream=None, track_uncommitted=True, **kwargs):
+        #this is so we can view objects and then easily commit
+        if track_uncommitted:
+            self.deserialized_objects = deque()
+        else:
+            self.deserialized_objects = None
+        super(DocumentDataTap, self).__init__(instream, **kwargs)
     
-    def __init__(self, *collection_sources, **kwargs):
-        '''
-        :param collection_sources: Maybe a Document, a Document instance, a Document queryset or a list of Document instances.
-        '''
-        self.collection_sources = collection_sources or []
-        super(DocumentDataTap, self).__init__(**kwargs)
+    def get_domain(self):
+        if self.instream is None: #no instream, I guess we write?
+            return 'deserialized_document'
+        if isinstance(self.instream, (list, tuple)):
+            return 'primitive'
+        if self.instream.domain == 'primitive':
+            return 'deserialized_document'
     
-    def get_raw_item_stream(self, filetap=None):
-        '''
-        Yields objects from the collection sources
-        '''
-        for source in self.collection_sources:
+    def get_instance_stream(self, instream):
+        for source in instream:
             try:
                 is_doc = issubclass(source, schema.Document)
                 is_instance = False
@@ -104,16 +57,41 @@ class DocumentDataTap(DataTap):
             for item in queryset:
                 yield item
     
-    def write_item(self, item):
+    def get_primitive_stream(self, instream):
         '''
-        Creates and returns a document instance
+        Convert various model sources to primitive objects
         '''
-        result = Deserializer([item]).next()
-        result.save()
-        return result.object
+        serializer = FileAwareSerializer()
+        instances = self.get_instance_stream(instream)
+        return serializer.serialize(instances)
+    
+    def get_deserialized_document_stream(self, instream):
+        '''
+        Convert primitive objects to deserialized document instances
+        '''
+        for deserialized_object in Deserializer(instream):
+            if self.deserialized_objects is not None:
+                self.deserialized_objects.append(deserialized_object)
+            yield deserialized_object
+    
+    def get_document_stream(self, instream):
+        '''
+        Convert primitive objects to saved document instances
+        '''
+        for item in self.get_deserialized_document_stream(instream):
+            item.save()
+            yield item
+    
+    def commit(self):
+        while self.deserialized_objects:
+            instance = self.deserialized_objects.popleft()
+            instance.save()
+        self.deserialized_objects = None
+        for instance in self:
+            instance.save()
     
     @classmethod
-    def load_from_command_line(cls, arglist):
+    def load_from_command_line(cls, arglist, instream=None):
         '''
         usage::
         
@@ -128,6 +106,6 @@ class DocumentDataTap(DataTap):
             else:
                 #get docs from appname
                 document_sources.extend(get_documents(arg))
-        return cls(*document_sources, **options.__dict__)
+        return cls(instream=document_sources, **options.__dict__)
 
 register_datatap('Document', DocumentDataTap)
--- dockit/tests/datatap_tests.py
+++ dockit/tests/datatap_tests.py
@@ -1,5 +1,7 @@
 from django.utils import unittest
 
+from datatap.datataps import MemoryDataTap
+
 from dockit import schema
 from dockit.datataps import DocumentDataTap
 
@@ -10,27 +12,30 @@ class SimpleDocument(schema.Document):
     featured = schema.BooleanField()
 
 class DocumentDataTapTestCase(unittest.TestCase):
-    def test_write_item(self):
-        tap = DocumentDataTap()
-        tap.open('w')
-        result = tap.write_item({
+    def test_load_item(self):
+        SimpleDocument.objects.all().delete()
+        source = MemoryDataTap([{
             'collection': SimpleDocument._meta.collection,
             'fields': {
                 'charfield': 'testchar',
                 'published': True,
                 'featured': False,
             }
-        })
-        self.assertTrue(isinstance(result, SimpleDocument))
+        }])
+        tap = DocumentDataTap(instream=source)
+        result = list(tap)
+        self.assertTrue(len(result), 1)
+        self.assertTrue(hasattr(result[0], 'save'))
+        tap.commit() #this saves said objects
         tap.close()
+        self.assertEqual(SimpleDocument.objects.all().count(), 1)
     
     def test_get_item_stream(self):
         SimpleDocument.objects.all().delete()
         SimpleDocument(charfield='testchar').save()
         
-        tap = DocumentDataTap(SimpleDocument)
-        tap.open('r')
-        items = list(tap.get_item_stream())
+        tap = DocumentDataTap(instream=[SimpleDocument])
+        items = list(tap)
         self.assertTrue(items)
         self.assertEqual(len(items), SimpleDocument.objects.all().count())
         assert len(items)
@@ -44,11 +49,11 @@ from datatap.management.commands import datatap
 
 
 class DocumentToZipCommandIntregrationTestCase(unittest.TestCase):
-    def test_dumpdatatap(self):
+    def test_document_to_zipfile(self):
         SimpleDocument(charfield='testchar').save()
         filename = mkstemp('zip', 'datataptest')[1]
         cmd = datatap.Command()
-        argv = ['manage.py', 'datatap', 'Document', SimpleDocument._meta.collection, '--', 'ZipFile', '--file', filename]
+        argv = ['manage.py', 'datatap', 'Document', SimpleDocument._meta.collection, '--', 'Zip', '--', 'File', filename]
         cmd.run_from_argv(argv)
         
         archive = zipfile.ZipFile(filename)
@@ -56,7 +61,7 @@ class DocumentToZipCommandIntregrationTestCase(unittest.TestCase):
         manifest = json.load(archive.open('manifest.json', 'r'))
         self.assertEqual(len(manifest), SimpleDocument.objects.all().count())
     
-    def test_loaddatatap(self):
+    def test_zipfile_to_document(self):
         SimpleDocument.objects.all().delete()
         item = {
             'collection': SimpleDocument._meta.collection,
@@ -73,9 +78,10 @@ class DocumentToZipCommandIntregrationTestCase(unittest.TestCase):
         archive.close()
         
         cmd = datatap.Command()
-        argv = ['manage.py', 'datatap', 'ZipFile', '--file', filename]
+        argv = ['manage.py', 'datatap', 'File', filename, '--', 'Zip', '--', 'Document']
         cmd.run_from_argv(argv)
         
         result = SimpleDocument.objects.all()
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0].charfield, 'testchar')
+
